587389fbff3f710ddb068c77db2392f1183b0185
avin madhu <84233047+avin-madhu@users.noreply.github.com>
2024-02-20 19:20:57 +0530

    auth test 3

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,21 +1,8 @@
 // Firebase Config
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";  // Update this line
 import "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";  // Add similar lines for other Firebase modules
 import "https://www.gstatic.com/firebasejs/10.8.0/firebase-functions.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAbzDGgeZCaiq-dmWQNqnpq_yi6KLyn_Q0",
-  authDomain: "miniproject-740e2.firebaseapp.com",
-  projectId: "miniproject-740e2",
-  storageBucket: "miniproject-740e2.appspot.com",
-  messagingSenderId: "493659960891",
-  appId: "1:493659960891:web:d8908059f91a37a6bc8e1e",
-  measurementId: "G-49BHFJDWQD"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
 // make auth and firestore references
 const auth = firebase.auth();
