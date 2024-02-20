4964446e12eb299bd2d45001d4e6a0465a5efb19
avin madhu <84233047+avin-madhu@users.noreply.github.com>
2024-02-20 20:50:50 +0530

    user

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -43,9 +43,7 @@ signupForm.addEventListener('submit', (e) => {
   const logout = document.querySelector('#logout');
     logout.addEventListener('click', (e) => {
         e.preventDefault();
-        auth.signOut().then(() => {
-        alert('user signed out');
-        });
+        auth.signOut()
     });
 
 
