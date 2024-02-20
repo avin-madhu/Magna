f6310c407d5322f023f6ff249e8971b589cf0ff3
avin madhu <84233047+avin-madhu@users.noreply.github.com>
2024-02-20 22:15:36 +0530

    conditional Links

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -12,12 +12,29 @@ const functions = firebase.functions();
 // update firestore settings
 db.settings({ timestampsInSnapshots: true });
 
+//conditional Links
+const loggedOutLinks = document.querySelectorAll('.logged-out');
+const loggedInLinks = document.querySelectorAll('.logged-in');
+
+const setupUI = (user) => {
+  if (user) {
+    loggedInLinks.forEach(item => item.style.display = 'block');
+    loggedOutLinks.forEach(item => item.style.display = 'none');
+  } else {
+    loggedInLinks.forEach(item => item.style.display = 'none');
+    loggedOutLinks.forEach(item => item.style.display = 'block');
+  }
+}
+
+
 
 auth.onAuthStateChanged(user => {
     if (user) {
         alert('user logged in: ', user);
+        setupUI(user);
     } else {
         alert('user logged out');
+        setupUI();
     }
 })
 
--- assets/js/main.js
+++ assets/js/main.js
@@ -247,24 +247,7 @@
   new PureCounter();
 
 
-   /**
-   * Making it act like a MUlti PAge Website
-   */
-//   function showSection(sectionId) {
-//     // Hide all sections
-//     var sections = document.querySelectorAll('section');
-//     sections.forEach(function (section) {
-//         section.classList.remove('active');
-//     });
-
-//     // Show the selected section
-//     var selectedSection = document.getElementById(sectionId);
-//     selectedSection.classList.add('active');
-// }
-
-// changing font Size
-
-
+  // Conditional Links
 
 
 })()
