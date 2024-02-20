a39aaa8cd1572e28d42b387a6ffb427ef802b007
avin madhu <84233047+avin-madhu@users.noreply.github.com>
2024-02-20 19:46:10 +0530

    login

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -30,12 +30,28 @@ signupForm.addEventListener('submit', (e) => {
 
 
   // Logout or Sign Out
-
   const logout = document.querySelector('#logout');
     logout.addEventListener('click', (e) => {
         e.preventDefault();
         auth.signOut().then(() => {
         console.log('user signed out');
+        });
+    });
+
+
+  // Login Users
+    const loginForm = document.querySelector('#login-form');
+    loginForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const email = loginForm['login-email'].value;
+        const password = loginForm['login-password'].value;
+
+        auth.signInWithEmailAndPassword(email, password).then((cred) => {
+        console.log("user Logged In", cred.user);
+        const modal = document.querySelector('#login-modal');
+        modal.hide();
+        loginForm.reset();
         });
     });
 
