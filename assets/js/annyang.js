d82f687f4f53df47c8aceed00ccb2990269ce829
avin madhu <84233047+avin-madhu@users.noreply.github.com>
2024-02-20 18:43:17 +0530

    firebase auth

--- assets/js/annyang.js
+++ assets/js/annyang.js
@@ -1,21 +1,24 @@
+window.onload = function()
+{    
+    if (annyang) {
+      // Let's define our first command. First the text we expect, and then the function it should call
+      var commands = {
+        'Hello': function() {
+          alert('Hello world!');
+        },
+        'About': function() {
+          window.location.href = '#about';
+        },
+        'go back up': function() {
+          window.location.href = '#';
+        },
+      };
 
-if (annyang) {
-  // Let's define our first command. First the text we expect, and then the function it should call
-  var commands = {
-    'Hello': function() {
-      alert('Hello world!');
-    },
-    'About': function() {
-      window.location.href = '#about';
-    },
-    'go back up': function() {
-      window.location.href = '#';
-    },
-  };
+      // Add our commands to annyang
+      annyang.addCommands(commands);
 
-  // Add our commands to annyang
-  annyang.addCommands(commands);
+      // Start listening. You can call this here, or attach this call to an event, button, etc.
+      annyang.start();
+    }
 
-  // Start listening. You can call this here, or attach this call to an event, button, etc.
-  annyang.start();
 }
---
+++ assets/js/auth.js
@@ -0,0 +1,40 @@
+// Firebase Config
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAbzDGgeZCaiq-dmWQNqnpq_yi6KLyn_Q0",
+  authDomain: "miniproject-740e2.firebaseapp.com",
+  projectId: "miniproject-740e2",
+  storageBucket: "miniproject-740e2.appspot.com",
+  messagingSenderId: "493659960891",
+  appId: "1:493659960891:web:d8908059f91a37a6bc8e1e",
+  measurementId: "G-49BHFJDWQD"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// make auth and firestore references
+const auth = firebase.auth();
+const db = firebase.firestore();
+const functions = firebase.functions();
+
+// update firestore settings
+db.settings({ timestampsInSnapshots: true });
+
+// Sign Up
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit', (e) => {
+  e.preventDefault(); 
+
+  const email = signupForm['signup-email'].value;
+  const password = signupForm['signup-password'].value;
+
+  
+  auth.createUserWithEmailAndPassword(email, password).then(cred => {
+    console.log(cred.user);
+    signupForm.reset();
+  });
+}); 
