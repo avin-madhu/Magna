8b500bbe1d8db9cc5862c25c328904c409edee31
avin madhu <84233047+avin-madhu@users.noreply.github.com>
2024-02-20 19:24:07 +0530

    auth test 5

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -20,7 +20,11 @@ signupForm.addEventListener('submit', (e) => {
   const email = signupForm['signup-email'].value;
   const password = signupForm['signup-password'].value;
 
-  console.log(email, password);
+  auth.createUserWithEmailAndPassword(email, password).then(cred => {
+    console.log(cred.user);
+    const modal = document.querySelector('#modal-signup');
+    M.Modal.getInstance(modal).close();
+    signupForm.reset();
 
   });
 
